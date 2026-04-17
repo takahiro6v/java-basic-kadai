415faf9af2f9339ee70d274be4c206516fa2585d
takahiro6v <tanarita@aicom.co.jp>
2026-04-17 16:07:04 +0900

    Add files via upload

---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,34 @@
+package text.kadai_015;
+
+public class Car_Chapter15 {
+	private int gear = 1;
+	private int speed = 10;
+	
+	
+	
+	public void changeGear(int afterGear) {
+		int beforeGear = this.gear;
+		this.gear = afterGear;
+		
+		switch(this.gear) {
+			case 1 ->
+				this.speed = 10;
+			case 2 ->
+				this.speed = 20;
+			case 3 ->
+				this.speed = 30;
+			case 4 ->
+				this.speed = 40;
+			case 5 ->
+				this.speed = 50;
+			default ->
+				this.speed = 10;
+		}
+		
+		System.out.println("ギア" + beforeGear + "から" + this.gear + "に切り替えました");
+	}
+	
+	public void run() {
+		System.out.println("速度は時速" + this.speed + "km");
+	}
+}
